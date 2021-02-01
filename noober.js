85126d82537cf732fa0ae0583815abe87e551956
Alex Glover <alex.glover@kellogg.northwestern.edu>
2021-01-31 19:18:59 -0600

    access rider info from json array

--- noober.js
+++ noober.js
@@ -7,6 +7,26 @@ async function pageLoaded() {
   
   // 🔥 start here: write code to loop through the rides
   
+  // look at json structure -- it's an array with 100 rides. each ride is an array of objects, with each object containing rider data
+  //console.log(json)
+  console.log(json.length)
+  for(let i = 0; i < json.length; i++) {
+    let rideRequests = json[i]
+    console.log(rideRequests)
+    for(let ii = 0; ii < rideRequests.length; ii++) {
+      // isolates the individual objects within the rideRequests array. this represents the ride legs
+      let rideLegs = rideRequests[ii]
+      //console.log(rideLegs)
+      //console.log(rideLegs.length)
+      
+      // access details for each individual rider
+      let rideLegsPassengerDetails = rideLegs.passengerDetails.first
+      console.log(rideLegsPassengerDetails)
+      
+    }
+    //console.log(ride)
+  }
+
 }
 
 window.addEventListener('DOMContentLoaded', pageLoaded)
